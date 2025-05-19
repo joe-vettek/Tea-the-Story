c49e627c05e98a933ae61f588a813f19cdce3638
joe-vettek <my1233215@126.com>
2025-05-19 12:58:41 +0800

    fix manual

--- src/main/java/xueluoanping/teastory/manual/ManualWork.java
+++ src/main/java/xueluoanping/teastory/manual/ManualWork.java
@@ -1,17 +1,20 @@
 package xueluoanping.teastory.manual;
 
 import net.minecraft.world.item.Item;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.loading.FMLLoader;
 import xueluoanping.teastory.registry.ItemRegister;
 import xueluoanping.teastory.registry.ManualRegistry;
 
 public class ManualWork {
 
     public void load(IEventBus loadEventBus) {
-        ManualRegistry.MANUALS.register(loadEventBus);
-        ManualRegistry.DOCUMENT_PROVIDERS.register(loadEventBus);
-        ManualRegistry.PATH_PROVIDERS.register(loadEventBus);
-
+        if (FMLLoader.getDist() == Dist.CLIENT) {
+            ManualRegistry.MANUALS.register(loadEventBus);
+            ManualRegistry.DOCUMENT_PROVIDERS.register(loadEventBus);
+            ManualRegistry.PATH_PROVIDERS.register(loadEventBus);
+        }
         ManualRegistry.MANUAL_ITEM = ItemRegister.ModItems.register("manual", () -> new ManualItem(new Item.Properties()));
     }
 
--- src/main/java/xueluoanping/teastory/registry/ManualRegistry.java
+++ src/main/java/xueluoanping/teastory/registry/ManualRegistry.java
@@ -9,6 +9,8 @@ import li.cil.manual.api.provider.PathProvider;
 import li.cil.manual.api.util.Constants;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Tiers;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import xueluoanping.teastory.TeaStory;
@@ -18,15 +20,23 @@ import xueluoanping.teastory.manual.TManual;
 public class ManualRegistry {
 
     public static final DeferredRegister<ManualModel> MANUALS = DeferredRegister.create(Constants.MANUAL_REGISTRY, TeaStory.MODID);
-    public static final RegistryObject<TManual> MANUAL = MANUALS.register("manual", TManual::new);
+    public static final RegistryObject<TManual> MANUAL =
+            FMLLoader.getDist() == Dist.CLIENT?
+            MANUALS.register("manual", TManual::new):null;
 
     public static final DeferredRegister<DocumentProvider> DOCUMENT_PROVIDERS = DeferredRegister.create(Constants.DOCUMENT_PROVIDER_REGISTRY, TeaStory.MODID);
-    public static final RegistryObject<DocumentProvider> DOCUMENT_PROVIDER = DOCUMENT_PROVIDERS.register("manual_provider", () ->
-            new NamespaceDocumentProvider(TeaStory.MODID, "doc"));
+    public static final RegistryObject<DocumentProvider> DOCUMENT_PROVIDER =
+            FMLLoader.getDist() == Dist.CLIENT?
+            DOCUMENT_PROVIDERS.register("manual_provider", () ->
+            new NamespaceDocumentProvider(TeaStory.MODID, "doc")):
+            null;
 
     public static final DeferredRegister<PathProvider> PATH_PROVIDERS = DeferredRegister.create(Constants.PATH_PROVIDER_REGISTRY, TeaStory.MODID);
-    public static final RegistryObject<PathProvider> PATH_PROVIDER = PATH_PROVIDERS.register("manual_path_provider", () ->
-            new NamespacePathProvider(TeaStory.MODID, false));
+    public static final RegistryObject<PathProvider> PATH_PROVIDER =
+            FMLLoader.getDist() == Dist.CLIENT?
+            PATH_PROVIDERS.register("manual_path_provider", () ->
+            new NamespacePathProvider(TeaStory.MODID, false)):
+            null;
 
     public static RegistryObject<Item> MANUAL_ITEM = null;
 
